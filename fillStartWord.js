f8cb094568115c3976dff73c23c0d6e07060aca8
Anon1166 <anon.2222@mail.kmutt.ac.th>
2022-09-14 22:06:45 +0700

    V6

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,14 +1,16 @@
-const { template } = require('@babel/core')
+//const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
   let a = startWord;
   let b = word;
   if(word == null || word == undefined ){
     return undefined;
-  }else if(b.startsWith(a)){
+  }else if(b.startsWith(a)){ //หรือใช้ startsWith
     return b;
   }else {
     return a+b;
   }
 }
-module.exports = fillStartWord
+//module.exports = fillStartWord
+console.log(fillStartWord("first","firststep"));
+
